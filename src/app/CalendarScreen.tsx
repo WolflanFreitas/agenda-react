d185062b824642b61730e41ffc6c2095372e9ee7
WolflanFreitas <wolflanf@gmail.com>
2022-08-24 07:01:13 -0300

    cont

--- src/app/CalendarScreen.tsx
+++ src/app/CalendarScreen.tsx
@@ -1,35 +1,51 @@
+import { Box } from '@mui/material';
 import Table from '@mui/material/Table';
-
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
 import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 
-
-
+const estilo = {
+    root: {
+        height: "100%",
+    },
+    table: {
+        minHeight: "100vh",
+        "& td ~ td, & th ~ th": {
+            borderRight: "1px solid rgb(224, 224, 224)"
+        }
+    }
+}
 const DAYS_OF_WEEK = ['DOM', 'SEG', 'TER', 'QUA', 'QUI', 'SEX', 'SÁB'];
 
 export function CalendarScreen() {
-    const classes = styles;
-    return <TableContainer component={"div"}>
-        <Table aria-label="a dense table">
-            <TableHead>
-                <TableRow>
-                    {DAYS_OF_WEEK.map(day => { return <TableCell align="center">{day}</TableCell> })}
-                </TableRow>
-            </TableHead>
-            <TableBody>
-                <TableRow>
-                    {DAYS_OF_WEEK.map(day => { return <TableCell align="center">x</TableCell> })}
-                </TableRow>
-                <TableRow>
-                    {DAYS_OF_WEEK.map(day => { return <TableCell align="center">x</TableCell> })}
-                </TableRow>
-                <TableRow>
-                    {DAYS_OF_WEEK.map(day => { return <TableCell align="center">x</TableCell> })}
-                </TableRow>
-            </TableBody>
-        </Table>
-    </TableContainer>
+
+    return (
+        <Box display="flex">
+            <Box>
+                <h2>Agenda React</h2>
+            </Box>
+            <TableContainer sx={estilo.root} component={"div"}>
+                <Table sx={estilo.table} size="small" aria-label="a dense table">
+                    <TableHead>
+                        <TableRow>
+                            {DAYS_OF_WEEK.map(day => { return <TableCell key={day} align="center">{day}</TableCell> })}
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        <TableRow>
+                            {DAYS_OF_WEEK.map(day => { return <TableCell key={day} align="center">x</TableCell> })}
+                        </TableRow>
+                        <TableRow>
+                            {DAYS_OF_WEEK.map(day => { return <TableCell key={day} align="center">x</TableCell> })}
+                        </TableRow>
+                        <TableRow>
+                            {DAYS_OF_WEEK.map(day => { return <TableCell key={day} align="center">x</TableCell> })}
+                        </TableRow>
+                    </TableBody>
+                </Table>
+            </TableContainer>
+        </Box>
+    )
 }
